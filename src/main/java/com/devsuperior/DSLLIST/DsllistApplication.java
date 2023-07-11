d7f8f6b0a242fd0406c3d561b29daf386dd72cea
Nicolas Barbosa <126782143+NicolasBarbosaa@users.noreply.github.com>
2023-07-11 00:51:48 -0300

    Add files via upload

---
+++ src/main/java/com/devsuperior/DSLLIST/DsllistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSLLIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsllistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsllistApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/DSLLIST/entites/Games.java
@@ -0,0 +1,128 @@
+package com.devsuperior.DSLLIST.entites;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+//configura para que ela seja equivalente a uma tabela do banco de dados relacional
+@Table(name= "tb__game")
+//
+public class Games {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String title;
+	
+	@Column(name= "game_year")
+	private Integer year;
+	private String genre;
+	private String plataform;
+	private String imgUrl;
+	private String shortDescription;
+	private String longdescription;
+	
+	public Games() {
+	}
+
+	public Games(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+			String shortDescription, String longdescription) {
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+		this.plataform = plataform;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+		this.longdescription = longdescription;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlataform() {
+		return plataform;
+	}
+
+	public void setPlataform(String plataform) {
+		this.plataform = plataform;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongdescription() {
+		return longdescription;
+	}
+
+	public void setLongdescription(String longdescription) {
+		this.longdescription = longdescription;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Games other = (Games) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
